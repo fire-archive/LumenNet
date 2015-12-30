c5fe1fb260e60d13272022e7aa64a25fe16af2d5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2015-12-29 16:27:13 -0800

    Fix another paths problem.

--- DependentExtensions/Autopatcher/AutopatcherPostgreRepository/AutopatcherPostgreRepository.cpp
+++ DependentExtensions/Autopatcher/AutopatcherPostgreRepository/AutopatcherPostgreRepository.cpp
@@ -1826,7 +1826,7 @@ bool AutopatcherPostgreRepository2::UpdateApplicationFiles(const char *applicati
 				return false;
 			}
 
-			printf("%i/%i.%i/%i DIFF from %s to %s ...", fileListIndex+1, newFiles.fileList.Size(), rowIndex+1, numRows, pathToOldContent.c_str(), pathToNewContent.c_str());
+			printf("%i/%i.%i/%i DIFF from %s to %s ...", fileListIndex+1, newFiles.fileList.Size(), rowIndex+1, numRows, pathToOldContentU8.c_str(), pathToNewContentU8.c_str());
 			int patchAlgorithm;
 			int makePatchResult=MakePatch(pathToOldContentU8.c_str(), pathToNewContentU8.c_str(), &patch, &patchLength, &patchAlgorithm);
 			if (makePatchResult < 0 || makePatchResult == 2)
